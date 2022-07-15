8179dd9bee1510c4135911e3288a00f5608bba1d
SerraZ3 <serra.henrique3@gmail.com>
2022-07-14 21:54:59 -0300

    adicionando console.log

--- src/controllers/IndexController.js
+++ src/controllers/IndexController.js
@@ -1,9 +1,10 @@
 const indexController = {
   home: (req, res) => {
+    console.log(req.session.email);
     return res.render("index", {
       title: "Home",
       user: req.cookies.user,
-      admin: req.cookies.admin
+      admin: req.cookies.admin,
     });
   },
 };
